15b3f6aab6933be6fc7fb7c1a293dcb7528000ff
paulcastr0 <castropaul.learn@gmail.com>
2022-11-24 15:02:30 +0100

    Aggiungere i file di progetto.

---
+++ CSharpFunctions/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// ------------------  PROGRAMMA PRINCIPALE ---------------------------
+
+int[] arrayDiNumeri = { 5, 2, 3, 8, 10 };
+StampaArrayInteri(arrayDiNumeri);
+
+
+int Quadrato = 0;
+int[] ElevaArrayAlQuadrato = { };
+int sommaElementiArray = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// --------------- DICHIARAZIONE DELLE FUNZIONI ------------------------
+
+// Creo una funzione che stampa in console un array di interi
+void StampaArrayInteri(int[] arrayInteri)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < arrayInteri.Length; i++)
+    {
+        if (i == arrayInteri.Length - 1)
+        {
+            Console.Write(arrayInteri[i]);
+        }
+        else
+        {
+            Console.Write(arrayInteri[i] + ", ");
+        }
+    }
+
+    Console.WriteLine("]");
+}
