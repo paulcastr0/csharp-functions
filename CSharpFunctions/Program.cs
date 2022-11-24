6bdb707b574a7bc22764db8e42de1cb654a5779f
paulcastr0 <castropaul.learn@gmail.com>
2022-11-24 17:51:48 +0100

    Completamento Fase 2

--- CSharpFunctions/Program.cs
+++ CSharpFunctions/Program.cs
@@ -4,7 +4,7 @@
 
 
 
-int[] arrayDiNumeri = { 5, 2, 3, 8, 10 };
+int[] arrayDiNumeri = { 2, 6, 7, 5, 3, 9 };
 StampaArrayInteri(arrayDiNumeri);
 
 int number = 2;
@@ -12,8 +12,18 @@ Console.WriteLine("Il numero originale è: " + number);
 raddoppiaNumeroRef(ref number);
 Console.WriteLine("Il numero raddoppiato è: " + number);
 
+Console.WriteLine("Array di numeri originale: ");
+StampaArrayInteri(arrayDiNumeri);
 
+int[] arrayDiNumeriIncrementato = incrementoArrayNumeri(arrayDiNumeri);
 
+Console.WriteLine("Array di numeri originale: ");
+StampaArrayInteri(arrayDiNumeri);
+
+Console.WriteLine("Array di numeri incrementato originale: ");
+StampaArrayInteri(arrayDiNumeriIncrementato);
+
+Console.Write("La somma dei numeri originali è: " + SommaNumeri);
 
 // --------------- DICHIARAZIONE DELLE FUNZIONI ------------------------
 
@@ -48,83 +58,6 @@ void raddoppiaNumeroRef(ref int n)
     n = n * 2;
 }
 
-
-
-/*
-
-// --------------------------------------------------ESEMPIO---------------------------------------------------------------
-
-// ------------------  PROGRAMMA PRINCIPALE ---------------------------
-
-int[] arrayDiNumeri = { 5, 2, 3, 8, 10 };
-StampaArrayInteri(arrayDiNumeri);
-
-
-// ESEMPIO PASSAGGIO PER RIFERIMENTO
-int numero = 10;
-Console.WriteLine("Il numero originale è: " + numero);
-raddoppiaNumeroRef(ref numero);
-Console.WriteLine("Il numero raddoppiato è: " + numero);
-
-
-
-Console.WriteLine("Array di numeri originale: ");
-StampaArrayInteri(arrayDiNumeri);
-
-int[] arrayDiNumeriIncrementato = incrementoArrayNumeri(arrayDiNumeri);
-
-Console.WriteLine("Array di numeri originale: ");
-StampaArrayInteri(arrayDiNumeri);
-
-Console.WriteLine("Array di numeri incrementato originale: ");
-StampaArrayInteri(arrayDiNumeriIncrementato);
-
-
-
-
-// --------------- DICHIARAZIONE DELLE FUNZIONI ------------------------
-
-// Creo una funzione che stampa in console un array di interi
-void StampaArrayInteri(int[] arrayInteri)
-{
-    Console.Write("[");
-
-    for (int i = 0; i < arrayInteri.Length; i++)
-    {
-        if (i == arrayInteri.Length - 1)
-        {
-            Console.Write(arrayInteri[i]);
-        }
-        else
-        {
-            Console.Write(arrayInteri[i] + ", ");
-        }
-    }
-
-    Console.WriteLine("]");
-}
-// Ho creato una funzione il cui valore del parametro n viene passato come una copia dal programma principale.
-// Quindi modifiche al parametro n non si ripercuotono sulla variabile originale passata da fuori
-// Fate riferimento alle slide sul TYPE SYSTEM per vedere tutti i tipi valore il cui comportamente è il medesimo!
-// Es: [char, byte, bool, double, float, ...]
-int raddoppiaNumero(int n)
-{
-    n = n * 2;
-    return n;
-}
-
-
-// ATTENZIONE QUESTA FUNZIONE MODIFICA IL COMPORTAMENTO DI DEFAULT DI UNA VARIABILE DI TIPO VALORE
-// PASSATA NORMALMENTE COME VALORE (COPIA)
-// In questo caso non viene passata come una copia, ma come referenza della variabile originale che 
-// il programma principale passa!
-void raddoppiaNumeroRef(ref int n)
-{
-    n = n * 2;
-}
-
-
-
 // IMPORTANTE: qualunque tipo di array (quindi anche array di int, float, double, bool, ecc ecc) sono SEMPRE
 // passati come referenza. Dunque qualunque modifica sull'array fatta al parametro della funzione si 
 // ripercuote sull'array originale passato dal programma principale!
@@ -132,12 +65,11 @@ int[] incrementoArrayNumeriSenzaCopia(int[] arrayNumeri)
 {
     for (int i = 0; i < arrayNumeri.Length; i++)
     {
-        arrayNumeri[i] = arrayNumeri[i] + 1;
+        arrayNumeri[i] = arrayNumeri[i] * 2;
     }
 
     return arrayNumeri;
 }
-
 
 // Per evitare di sovrascrivere l'array originale passato dal programma principale, dovete voi occuparvi
 // quando create la funzione di copiare l'array originale in una nuova VARIABILE LOCALE!!!
@@ -148,17 +80,23 @@ int[] incrementoArrayNumeri(int[] arrayNumeri)
     int[] copiaArray = (int[])arrayNumeri.Clone();
     for (int i = 0; i < copiaArray.Length; i++)
     {
-        copiaArray[i] = copiaArray[i] + 1;
+        copiaArray[i] = copiaArray[i] * 2;
     }
 
     return copiaArray;
 }
 
 
-int SommaNumeri(int addendo1, int addendo2)
+int SommaNumeri() 
 {
-    int risultato = addendo1 + addendo2;
-    return risultato;
+    int sum = 0;
+    for (int i = 0; i < arrayDiNumeri.Length; i++)
+    {
+        sum += arrayDiNumeri[i];
+    }
+    return sum;
 }
 
-*/
+
+
+
